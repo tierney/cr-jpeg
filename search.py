26ded1f25a5b5060980ae1df8887569e38bf2ba3
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-05-08 19:08:25 -0400

    added control bins

--- search.py
+++ search.py
@@ -5,14 +5,15 @@ from jpegcodec import JPEG
 # 8x8 DCT tables of natural images
 # - most coefficients are between -200 and 600
 # - any coefficients in the first row are always positive 
-LOWEST = -200
-HIGHEST = 200 
 
 class BitEncoder:
-  def __init__(self, k, quant, bits_per_bin):
+  def __init__(self, k, r, quant, bits_per_bin, lowest, highest ):
     self.k = k
+    self.r = r 
     self.quant = quant
     self.bits_per_bin = bits_per_bin[:]
+    self.lowest = lowest 
+    self.highest = highest 
 
   def encode(self, bits):
     """
@@ -20,6 +21,7 @@ class BitEncoder:
     """
     assert len(bits) == self.k
     dct = np.zero_like(self.quant)
+    running_sum = 0
     for flat_idx in np.nonzero(self.bits_per_bin):
       b = self.bits_per_bin[flat_idx]
       substr = bits[curr_idx:curr_idx+b]
@@ -30,13 +32,18 @@ class BitEncoder:
       # the 2D index into quantization and DCT matrices
       idx = np.unravel_index(flat_idx, self.quant.shape)
       quantization_level = self.quant[idx]
-      v = n * quantization_level + (LOWEST if idx[0] > 0 else 0)
+      v = n * quantization_level + (lowest if idx[0] > 0 else 0)
+      running_sum += v
       dct[idx] = v 
     return dct
 
 
+import math 
 
-def search(quality = 20):
+
+# limits and target sum derived from empirical stats of some random
+# image of a kid on flickr
+def search(quality = 25, n_control_bins = 10, lowest = -348, highest=144, target_sum = -1000, target_std = 1300):
   """
   Inputs:
     - quality_level : int (between 1 and 100)
@@ -46,14 +53,15 @@ def search(quality = 20):
   """
   codec = JPEG()
   quant = codec.scaled_luminance_quant_matrix(quality)
-  quant_values = quant[:]
+  quant_values = np.ravel(quant)
   quant_shape = quant.shape
   sorted_indices = np.argsort(quant_values)
+  value_range = highest - lowest 
   bits_per_bin = np.zeros_like(quant_values, dtype='int')
   for i,idx in enumerate(sorted_indices):
-    q = quant_values[idx]
-    nbits = HIGHEST / q     
-    bits_per_bin[idx] = nbits
-  total_nbits = np.sum(bits_per_bin) 
+    q = float(quant_values[idx])
+    nbits = np.ceil(np.log2(value_range / q))
+    bits_per_bin[idx] = int(nbits)
+  total_nbits = np.sum(bits_per_bin[:-n_control_bins]) 
   encoder = BitEncoder(total_nbits, quant, bits_per_bin)
   return encoder
