397188dbc75238e67e4ded233217ad781fcc5a5f
Matt Tierney <tierney@cs.nyu.edu>
2012-05-11 07:41:54 -0400

    WIP levels analytically described.

--- YccLevels.py
+++ YccLevels.py
@@ -9,12 +9,15 @@ cs = ColorSpace()
 to_ycc = cs.to_ycc
 to_rgb = cs.to_rgb
 
+
 def valid_rgb_channel(val):
   return val >= 0 and val <= 255
+
 
 def enumerate_color_space():
   a = numpy.fromfunction(to_rgb, (256, 256, 256))
   return a
+
 
 def _numpy_array():
   rgb = enumerate_color_space()
@@ -24,8 +27,7 @@ def _numpy_array():
   print red[lum][cb][cr], green[lum][cb][cr], blue[lum][cb][cr]
 
 
-def create_top_k_list(values):
-  # values is a list where the index is what we want to keep track of.
+def chrominance_at_luminance():
   pass
 
 def main(argv):
@@ -33,15 +35,24 @@ def main(argv):
   for cb in range(256):
     for cr in range(256):
       red, green, blue = to_rgb(128, cb, cr)
-      if valid_rgb_channel(red) and valid_rgb_channel(green) and valid_rgb_channel(blue):
+      if valid_rgb_channel(red) and valid_rgb_channel(green) and \
+            valid_rgb_channel(blue):
         valid_limits.append((128, cb, cr))
   print 'Pairwise Distances'
   pd = pdist(valid_limits)
+  num_limits = len(valid_limits)
 
   print 'Organizing pdist'
   top_values = heapq.nlargest(32, pd)
-  for top_value in top_values:
-    print top_value, numpy.where(pd == top_value)
+  for top_value in set(top_values):
+    _values = numpy.where(pd == top_value)
+    print top_value, _values
+
+    # for value in _values[0]:
+
+    #   values / num_limits
+    #   print valid_limits[value],
+
 
 if __name__ == '__main__':
   main(sys.argv)
